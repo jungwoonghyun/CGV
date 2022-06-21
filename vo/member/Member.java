1a9c8483746c44109b0b19ca0780dde5a34e62c1
JungWoongHyun <wh5716@naver.com>
2022-06-21 18:00:09 +0900

    Rename package & add User info

---
+++ vo/member/Member.java
@@ -0,0 +1,19 @@
+package vo.member;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+
+    private String userID;
+	private String userPW;
+	private String userName;
+	private String userBirth;
+	private String userPhone;
+
+
+	public Member() {
+		super();
+	}
+
+    
+}
