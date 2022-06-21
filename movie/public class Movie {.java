959c09dd13a4d5b0e058b380d8da667e109f9d84
JungWoongHyun <wh5716@naver.com>
2022-06-21 11:09:47 +0900

    Make function showing movielist

--- movie/public class Movie {.java
+++ movie/public class Movie {.java
@@ -1,11 +1,22 @@
 package movie;
 
 public class Movie {
-    String[] film= {"1917","영화1","영화2"};
+    String[] film= {"탑건 메버릭","헤어질 결심","범죄도시2"};
 	int row =6;
 	int column =7;
     int costs[] = {11000,12500,15000};
 	float price=0;
     float dc=0;
 	String buyway[]= {"현장","인터넷","쿠폰"};
+
+    public void moviename() {
+        System.out.println("Choose number of movie");
+        System.out.println("-----------------");
+            for(int i=0;i<film.length;i++) {
+                System.out.print(i+1);
+                System.out.println(". "+film[i]);
+            }
+    }
+
+    
 }
