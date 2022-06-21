99603695d89b37e86e4c16a739c2a95ebe9c38da
JungWoongHyun <wh5716@naver.com>
2022-06-21 11:04:38 +0900

    First commit(add variables)

--- movie/public class Movie {.java
+++ movie/public class Movie {.java
@@ -1,5 +1,11 @@
 package movie;
 
 public class Movie {
-    
+    String[] film= {"1917","영화1","영화2"};
+	int row =6;
+	int column =7;
+    int costs[] = {11000,12500,15000};
+	float price=0;
+    float dc=0;
+	String buyway[]= {"현장","인터넷","쿠폰"};
 }
