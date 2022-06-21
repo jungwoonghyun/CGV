8ebb74e1312339b20b91123e9c497bb3152c0e95
JungWoongHyun <wh5716@naver.com>
2022-06-21 18:02:02 +0900

    Make functions about UserInfo

--- vo/member/Member.java
+++ vo/member/Member.java
@@ -15,5 +15,54 @@ public class Member implements Serializable {
 		super();
 	}
 
-    
+    public Member(String userID, String userPW, String userName, String userBirth, String userPhone) {
+		super();
+		this.userID = userID;
+		this.userPW = userPW;
+		this.userName = userName;
+		this.userBirth = userBirth;
+		this.userPhone = userPhone;
+		
+	}
+
+
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Member [userID=" + userID + ", userPW=" + userPW + ", userName=" + userName + ", userBirth=" + userBirth
+				+ ", userPhone=" + userPhone + "]";
+	}
+
 }
